83a4be57b96ea2b6b2acb86ef4d9183d829e2de4
Maxime Bruel <maximesare@live.fr>
2020-12-05 21:55:19 +0100

    Ajout de "conversion" (beta)

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// AJAX
+const getDataFromRGB = async () => {
+	
+	// verification du formulaire
+	if(document.querySelector('input[name="r"]').value != "" && document.querySelector('input[name="g"]').value != "" && document.querySelector('input[name="b"]').value != "") {
+
+		// affichage du loader
+		document.querySelector('#loader').classList.remove("hidden");
+
+		// recuperation des valeurs saisies
+		let r = document.querySelector('input[name="r"]').value;
+		let g = document.querySelector('input[name="g"]').value;
+		let b = document.querySelector('input[name="b"]').value;
+		let rgb = r+','+g+','+b;
+
+		// appel de l'API
+		const response = await fetch('https://cors-anywhere.herokuapp.com/http://thecolorapi.com/id?hex='+''+'&rgb='+rgb+'&format=json');
+		const data = await response.json();
+		try {
+
+			// cache du loader
+			document.querySelector('#loader').classList.add("hidden");
+
+			// affichage des resultats
+			document.querySelector('#conversion-result').classList.remove("hidden");
+			document.querySelector('#conversion-result div.result-hex p').innerHTML = data.hex.value;
+			document.querySelector('#conversion-result div.result-rgb p').innerHTML = data.rgb.value;
+			document.querySelector('#conversion-result div.result-cmyk p').innerHTML = data.cmyk.value;
+			document.querySelector('#conversion-result div.result-hsl p').innerHTML = data.hsl.value;
+			document.querySelector('#conversion-result div.result-hsv p').innerHTML = data.hsv.value;
+			document.querySelector('#color-sample').style.backgroundColor = data.hex.value;
+		}
+		catch(e) {
+			console.log('error', e);
+		}
+	}
+}
+
+const convert_btn = document.querySelector('#rgb-convert-btn');
+convert_btn.addEventListener('click', getDataFromRGB);
