c815fc479b10582d84f7372ec2e43f8da97571d3
Maxime Bruel <maximesare@live.fr>
2020-12-05 23:43:49 +0100

    Amelioration de "conversion"

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-// AJAX
-const getDataFromRGB = async () => {
+// thecolorapi
+const getData = async (from) => {
 	
 	// verification du formulaire
 	if(document.querySelector('input[name="r"]').value != "" && document.querySelector('input[name="g"]').value != "" && document.querySelector('input[name="b"]').value != "") {
@@ -7,35 +7,43 @@ const getDataFromRGB = async () => {
 		// affichage du loader
 		document.querySelector('#loader').classList.remove("hidden");
 
-		// cache de la couleur precedente
+		// on cache les eventuels resultats precedents
 		document.querySelector('#conversion-result').classList.add("hidden");
 
-		// recuperation des valeurs saisies
-		let r = document.querySelector('input[name="r"]').value;
-		let g = document.querySelector('input[name="g"]').value;
-		let b = document.querySelector('input[name="b"]').value;
-		let rgb = r+','+g+','+b;
+		// detection du format de couleur donne par l'utilisateur
+		if(from == "rgb") {
 
-		// appel de l'API
-		const response = await fetch('https://cors-anywhere.herokuapp.com/http://thecolorapi.com/id?hex='+''+'&rgb='+rgb+'&format=json');
+			// recuperation des valeurs saisies
+			let r = document.querySelector('input[name="r"]').value;
+			let g = document.querySelector('input[name="g"]').value;
+			let b = document.querySelector('input[name="b"]').value;
+			let rgb = r+','+g+','+b;
+
+			// ecriture de l'url d'appel de l'API
+			var response = await fetch('https://cors-anywhere.herokuapp.com/http://thecolorapi.com/id?hex='+''+'&rgb='+rgb+'&format=json');
+		}
+
 		const data = await response.json();
+
 		try {
 
-			// cache du loader
+			// on cache le loader
 			document.querySelector('#loader').classList.add("hidden");
 
 			// affichage des resultats
+
 			console.log(data);
 
+			document.querySelector('#conversion-result h2').innerHTML = data.name.value;
+			document.querySelector('#color-sample').style.backgroundColor = data.hex.value;
 			document.querySelector('#conversion-result').classList.remove("hidden");
 			document.querySelector('#conversion-result div.result-hex p').innerHTML = data.hex.value;
 			document.querySelector('#conversion-result div.result-rgb p').innerHTML = data.rgb.value;
 			document.querySelector('#conversion-result div.result-cmyk p').innerHTML = data.cmyk.value;
 			document.querySelector('#conversion-result div.result-hsl p').innerHTML = data.hsl.value;
 			document.querySelector('#conversion-result div.result-hsv p').innerHTML = data.hsv.value;
-			document.querySelector('#color-sample').style.backgroundColor = data.hex.value;
-			document.querySelector('#conversion-result h2').innerHTML = data.name.value;
 		}
+
 		catch(e) {
 			console.log('error', e);
 		}
@@ -43,4 +51,8 @@ const getDataFromRGB = async () => {
 }
 
 const convert_btn = document.querySelector('#rgb-convert-btn');
-convert_btn.addEventListener('click', getDataFromRGB);
+convert_btn.addEventListener('click', () => {
+	getData("rgb");
+});
+
+
